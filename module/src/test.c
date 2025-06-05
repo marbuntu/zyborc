a422e38e548cd911322e588a97c0fe9922d5c2bf
marbu <martin.anschuetz@vert-tec.io>
2025-06-05 17:57:48 +0200

    zephyr module setup

---
+++ module/src/test.c
@@ -0,0 +1,12 @@
+/**
+ * @brief   
+ * 
+ * @file    test.c
+ * 
+ * @author  M. Anschuetz ( martin.anschuetz@vert-tec.io )
+ * @version 0.1
+ * @date    2025-06-05
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
